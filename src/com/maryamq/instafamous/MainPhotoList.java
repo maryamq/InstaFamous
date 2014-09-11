58d83977ddb5aed1f747349078d1758871bfabc4
Maryam Quadir <maryamquadir@gmail.com>
2014-09-11 15:28:00 -0700

    Empty Files

---
+++ src/com/maryamq/instafamous/MainPhotoList.java
@@ -0,0 +1,15 @@
+package com.maryamq.instafamous;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainPhotoList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_photo_list);
+    }
+}

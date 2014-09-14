3a89cb930511ea12cbb32a438d5d39c08cf7ca57
Maryam Quadir <maryamquadir@gmail.com>
2014-09-14 11:34:39 -0700

    UI fixes.

--- gen/com/maryamq/instafamous/R.java
+++ gen/com/maryamq/instafamous/R.java
@@ -34,7 +34,9 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int caption_default_text=0x7f040002;
+        public static final int comments_default_text=0x7f040006;
         public static final int hello_world=0x7f040001;
+        public static final int likes_default_caption=0x7f040005;
         public static final int location_default_text=0x7f040004;
         public static final int userName_default_text=0x7f040003;
     }
